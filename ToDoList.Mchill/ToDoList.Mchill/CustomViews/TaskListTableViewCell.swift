aa80934b43236bb1b6b30b72fcdccc01db39ed3c
Matthew Hill <matthill265@gmail.com>
2023-02-19 19:05:13 -0700

    Started my TaskListTableViewCell and made all the outlets and actions. Set the numberOfRows on the ToDoListStepsTBVC to the count of tasks and optional casted the cell to be a TaskListTableViewCell

--- ToDoList.Mchill/ToDoList.Mchill/CustomViews/TaskListTableViewCell.swift
+++ ToDoList.Mchill/ToDoList.Mchill/CustomViews/TaskListTableViewCell.swift
@@ -9,7 +9,26 @@ import UIKit
 
 class TaskListTableViewCell: UITableViewCell {
 
+    // MARK: - Outlets
     
+    @IBOutlet weak var taskNameLabel: UILabel!
+    
+    @IBOutlet weak var taskCompletedButton: UIButton!
+    
+    // MARK: - functions
+    
+    func updateViews(task: Task) {
+        taskNameLabel.text = task.name
+        
+        let imageName = task.isComplete ? "checkmark.square.fill" : "checkmark.square"
+        taskCompletedButton.setImage(UIImage(systemName: imageName), for: .normal)
+    }
+    
+    
+    // MARK: - Actions
+    
+    @IBAction func taskCompletedButtonTapped(_ sender: Any) {
+    }
     
     
 }
