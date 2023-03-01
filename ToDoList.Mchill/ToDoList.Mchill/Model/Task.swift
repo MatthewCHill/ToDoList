04e39cbcc8164f816cd1e5393c702907d8493d7a
Matthew Hill <matthill265@gmail.com>
2023-03-01 07:29:35 -0700

    Made the madel files and the ViewControllers

---
+++ ToDoList.Mchill/ToDoList.Mchill/Model/Task.swift
@@ -0,0 +1,8 @@
+//
+//  Task.swift
+//  ToDoList.Mchill
+//
+//  Created by Matthew Hill on 3/1/23.
+//
+
+import Foundation
---
+++ ToDoList.Mchill/ToDoList.Mchill/Model/ToDoList.swift
@@ -0,0 +1,8 @@
+//
+//  ToDoList.swift
+//  ToDoList.Mchill
+//
+//  Created by Matthew Hill on 3/1/23.
+//
+
+import Foundation
