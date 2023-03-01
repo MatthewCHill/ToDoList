422e69dc82913e26ee2840157d0d18cc5bef052d
Matthew Hill <matthill265@gmail.com>
2023-03-01 07:47:26 -0700

    Created my SSOT and made my method signatures

---
+++ ToDoList.Mchill/ToDoList.Mchill/ModelControllers/TaskController.swift
@@ -0,0 +1,8 @@
+//
+//  TaskController.swift
+//  ToDoList.Mchill
+//
+//  Created by Matthew Hill on 3/1/23.
+//
+
+import Foundation
---
+++ ToDoList.Mchill/ToDoList.Mchill/ModelControllers/ToDoListController.swift
@@ -0,0 +1,30 @@
+//
+//  ToDoListController.swift
+//  ToDoList.Mchill
+//
+//  Created by Matthew Hill on 3/1/23.
+//
+
+import Foundation
+
+class ToDoListController {
+    
+    // MARK: - Singleton
+    static let shared = ToDoListController()
+    // MARK: - SSOT
+    var toDoList: [ToDoList] = []
+    
+    // MARK: - CRUD FUNCTIONS
+    
+    func createList() {
+        
+    }
+    
+    func deleteList() {
+        
+    }
+    
+    func toggleIsImportant() {
+        
+    }
+}
