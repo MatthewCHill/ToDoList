9f71a686059834ae82b582da19b809263f34baf2
Matthew Hill <matthill265@gmail.com>
2023-03-01 07:38:18 -0700

    made task equatable and gadded a id of type uuid

--- ToDoList.Mchill/ToDoList.Mchill/Model/Task.swift
+++ ToDoList.Mchill/ToDoList.Mchill/Model/Task.swift
@@ -10,15 +10,17 @@ import Foundation
 class Task {
     let name: String
     let isComplete: Bool
+    let id: UUID
     
-    init(name: String, isComplete: Bool = false) {
+    init(name: String, isComplete: Bool = false, id: UUID = UUID()) {
         self.name = name
         self.isComplete = isComplete
+        self.id = id
     }
 }
 extension Task: Equatable {
     static func == (lhs: Task, rhs: Task) -> Bool {
-        
+        return lhs.id == rhs.id
     }
     
     
