afdc46e3578a3c5e6450cfecb7f07144ceb03f13
Matthew Hill <matthill265@gmail.com>
2023-03-22 09:08:19 -0600

    Creates the files for the custom cells

---
+++ ToDoList.Mchill/ToDoList.Mchill/ViewControllers/CustomViews/ToDoListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ToDoListTableViewCell.swift
+//  ToDoList.Mchill
+//
+//  Created by Matthew Hill on 3/22/23.
+//
+
+import UIKit
+
+class ToDoListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
